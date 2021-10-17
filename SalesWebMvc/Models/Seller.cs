0588030ca88d118ec676feddc529a2c65f828588
Leonardo-Da-Silva-Rocha <leosilvarocha.lsr@gmail.com>
2021-10-17 18:21:13 -0300

    Objetos formatados

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,17 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+        [DataType(DataType.EmailAddress)]
         public String Email { get; set; }
         public String Nome { get; set; }
+
+        [Display(Name = "Base Salary")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public Double BaseSalary { get; set; }
+
+        [Display(Name = "Birth Date")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
         public Departament Departament { get; set; }
         public int DepartamentId { get; set; }
